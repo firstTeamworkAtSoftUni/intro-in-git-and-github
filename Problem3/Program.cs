1a8af92b9c81b4c086fc66d4deb323804af43c9c
hrdondabeat <hrdlghrdlg@gmail.com>
2018-03-25 17:19:51 +0300

    Added Problem3 to repo

---
+++ Problem3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var volume = int.Parse(Console.ReadLine());
+            var energy = int.Parse(Console.ReadLine());
+            var sugarContent = int.Parse(Console.ReadLine());
+
+            var calories = (double)volume * energy / 100.0;
+            var sugar = volume * sugarContent / 100.0;
+
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{calories}kcal, {sugar}g sugars");
+
+        }
+    }
+}
