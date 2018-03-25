6bd5386241e9f8829a199cfc5fa37fd15be6c84a
hrdondabeat <37152292+hrdondabeat@users.noreply.github.com>
2018-03-25 17:33:02 +0300

    Update Program.cs

--- Problem2/Program.cs
+++ Problem2/Program.cs
@@ -9,11 +9,15 @@ namespace Rectangle_area
     class Program
     {
         static void Main(string[] args)
-        {
-            var width = float.Parse(Console.ReadLine());
+        {   
+            /* in the next two lines , we recieve rectangle width
+            and height as floats */ 
+            var width = float.Parse(Console.ReadLine()); 
             var height = float.Parse(Console.ReadLine());
+            /* we calculate rectangle area*/
             var area = width * height;
-
+            /* then we print the area result rounded to the 
+            second digit after the float point */
             Console.Write("{0:F2}", area);
         }
     }
