d0890c1c1801d7e4c970f4147f1ba818d173a3ca
martingganev <martoganev97@gmail.com>
2018-03-25 16:43:01 +0300

    Added comments to the ProblemFromVladd/Program.cs Updated the README.md Added the team.txt file Added Problem2 and the corresponding files

---
+++ Problem2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = float.Parse(Console.ReadLine());
+            var height = float.Parse(Console.ReadLine());
+            var area = width * height;
+
+            Console.Write("{0:F2}", area);
+        }
+    }
+}
--- ProblemFromVladd/Program.cs
+++ ProblemFromVladd/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//CONFIRMED from <martingganev>
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace P01
     {
         static void Main(string[] args)
         {
+            //We receive from the Console the number of inputs -> integer value and density -> double value.
             int n = int.Parse(Console.ReadLine());
             double density = double.Parse(Console.ReadLine());
             double sum = 0.0;
@@ -20,17 +22,21 @@ namespace P01
 
                 double raindropsCount = nums[0];
                 double squareMeters = nums[1];
+                //We receive input from the Console as raindropsCount -> double and squareMeters -> double.
                 sum += raindropsCount / squareMeters;
+                //We calculate the sum..
             }
 
             if (sum != 0 && density != 0)
             {
                 double totalSum = sum / density;
                 Console.WriteLine("{0:f3}", totalSum);
+                //Calculate the totalSum and print it on the Console.
             }
             else
             {
                 Console.WriteLine("{0:f3}", sum);
+                //Print the sum on the Console.
             }
         }
     }
